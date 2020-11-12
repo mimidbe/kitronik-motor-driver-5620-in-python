6c53da9be92fac0c820ed45465a5428a1b5b8a4a
mimidbe <51967427+mimidbe@users.noreply.github.com>
2020-11-12 22:17:10 +0100

    Update testServo.py

--- testServo.py
+++ testServo.py
@@ -4,6 +4,6 @@ import kitronik_motor_driver
 sv1 = KServo(pin0)
 while True:
     if button_a.is_pressed():
-        sv1.write_angle(50) # turn servo to 50 degrees 
+        sv1.writeAngle(50) # turn servo to 50 degrees 
     elif button_b.is_pressed():
-        sv1.write_angle(0) # return servo to 0 degrees 
+        sv1.writeAngle(0) # return servo to 0 degrees 
