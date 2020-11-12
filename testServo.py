7f3db1f3ad3f025d9824c99b5890cc83c20f9996
mimidbe <51967427+mimidbe@users.noreply.github.com>
2020-11-12 22:14:49 +0100

    Create testServo.py

---
+++ testServo.py
@@ -0,0 +1,9 @@
+from microbit import *
+import kitronik_motor_driver
+
+sv1 = KServo(pin0)
+while True:
+    if button_a.is_pressed():
+        sv1.write_angle(50) # turn servo to 50 degrees 
+    elif button_b.is_pressed():
+        sv1.write_angle(0) # return servo to 0 degrees 
