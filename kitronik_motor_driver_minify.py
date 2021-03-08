9725e01f8c0d00569a2f9106dff3c92dada8522c
mimidbe <51967427+mimidbe@users.noreply.github.com>
2021-03-08 14:45:41 +0100

    Update kitronik_motor_driver_minify.py

--- kitronik_motor_driver_minify.py
+++ kitronik_motor_driver_minify.py
@@ -1,25 +1,23 @@
-_D='reverse'
-_C='forward'
-_B='motor 2'
-_A='motor 1'
-from microbit import *
+G='motor 2'
+F='motor 1'
+from microbit import pin0 as A,pin8 as E,pin12 as B,pin16 as C
 class KMotor:
 	def __convert(self,x):return 1023*x//100
-	def motorOn(self,motor,dir,speed):
-		OutputVal=self.__convert(speed)
-		if motor==_A:
-			if dir==_C:pin8.write_analog(OutputVal);pin12.write_digital(0)
-			elif dir==_D:pin12.write_analog(OutputVal);pin0.write_digital(0)
-		elif motor==_B:
-			if dir==_C:pin0.write_analog(OutputVal);pin16.write_digital(0)
-			elif dir==_D:pin16.write_analog(OutputVal);pin0.write_digital(0)
-	def motorOff(self,motor):
-		if motor==_A:pin8.write_digital(0);pin12.write_digital(0)
-		elif motor==_B:pin0.write_digital(0);pin16.write_digital(0)
-	def motorBrake(self,motor):
-		if motor==_A:pin8.write_digital(1);pin12.write_digital(1)
-		elif motor==_B:pin0.write_digital(1);pin16.write_digital(1)
+	def motorOn(self,H,dir,S):
+		K='reverse';J='forward';D=self.__convert(S)
+		if H==F:
+			if dir==J:E.write_analog(D);B.write_digital(0)
+			elif dir==K:B.write_analog(D);A.write_digital(0)
+		elif H==G:
+			if dir==J:A.write_analog(D);C.write_digital(0)
+			elif dir==K:C.write_analog(D);A.write_digital(0)
+	def motorOff(self,D):
+		if D==F:E.write_digital(0);B.write_digital(0)
+		elif D==G:A.write_digital(0);C.write_digital(0)
+	def motorBrake(H,D):
+		if D==F:E.write_digital(1);B.write_digital(1)
+		elif D==G:A.write_digital(1);C.write_digital(1)
 class KServo:
-	def __init__(self,pin,freq=50,min_us=600,max_us=2400,angle=180):self.min_us=min_us;self.max_us=max_us;self.us=0;self.freq=freq;self.angle=angle;self.analog_period=0;self.pin=pin;analog_period=round(1/self.freq*1000);self.pin.set_analog_period(analog_period)
-	def __write_us(self,us):us=min(self.max_us,max(self.min_us,us));duty=round(us*1024*self.freq//1000000);self.pin.write_analog(duty);sleep(100);self.pin.write_digital(0)
-	def writeAngle(self,degrees=None):degrees=degrees%360;total_range=self.max_us-self.min_us;us=self.min_us+total_range*degrees//self.angle;self.__write_us(us)
+	def __init__(self,pin,freq=50,min_us=600,max_us=2400,angle=180):self.min_us=min_us;self.max_us=max_us;self.us=0;self.freq=freq;self.angle=angle;self.pin=pin;B=D(1/self.freq*1000);self.pin.set_analog_period(B)
+	def __write_us(self,us):us=min(self.max_us,max(self.min_us,us));B=round(us*1024*self.freq//1000000);self.pin.write_analog(B);sleep(100);self.pin.write_digital(0)
+	def writeAngle(self,degrees=None):B=degrees;B=B%360;C=self.max_us-self.min_us;D=A.min_us+C*B//self.angle;self.__write_us(D)
